7441714c13686d1ab62a7aecb159f10bd4381bb0
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-07-01 08:16:59 +0700

    main

---
+++ 01-Mengimplementasikan-User-Interface/action.js
@@ -0,0 +1,8 @@
+
+
+const tombol = document.querySelector('.tombol')
+const menu = document.querySelector('.menu')
+
+tombol.addEventListener('click', () => {
+    menu.classList.toggle('aktif')
+})
