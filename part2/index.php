d0782d7e6faf082149526ce09826fc878dc212dd
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-07-04 15:08:25 +0700

    main

---
+++ part2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
+<body>
+    <div class="container mt-5">
+        <div class="card w-md">
+            <div class="card-header">
+                <h1>hallo</h1>
+            </div>
+        </div>
+    </div>
+<script src="./js/jquery/jquery.min.js"></script>
+<script src="./js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
