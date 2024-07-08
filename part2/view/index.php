879acb135f195eb9e8806103c27b8ef49d1d0f5c
gemafajar09 <gemafajarramadhan09@gmail.com>
2024-07-08 16:32:29 +0700

    main

---
+++ part2/view/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../vendor/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="../vendor/css/bootstrap/bootstrap.min.css">
+</head>
+<body>
+    
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h5>Data Kegiatan</h5>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Foto</th>
+                            <th>Title</th>
+                            <th>Deskripsi</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>gema</td>
+                            <td>gema</td>
+                            <td>gema</td>
+                            <td>
+                                <button class="btn btn-primary">
+                                    Edit
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="../vendor/js/jquery.min.js"></script>
+    <script src="../vendor/js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
